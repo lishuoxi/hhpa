aac14b82a3eda6b4cbfc99f9c48491f4a595bef0
ll <11@qq.com>
2025-10-22 13:52:27 +0800

    new version

---
+++ api/app/Services/UidCallbackService.php
@@ -0,0 +1,14 @@
+<?php namespace App\Services;
+
+use DB;
+use Log;
+use App\Models\Trade;
+use Carbon\Carbon;
+
+class UidCallbackService
+{
+   static function doPay($trade, $channel)
+   {
+   }
+   
+}
